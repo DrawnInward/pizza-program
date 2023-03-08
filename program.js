d9668c5f7995ae7260ad862b8f25be0452669579
DrawnInward <jonathan_bean@hotmail.co.uk>
2023-03-08 16:36:57 +0000

    added comments explaining all the bits of code

--- program.js
+++ program.js
@@ -4,19 +4,20 @@
 * you need too sort out how the display current pizza and the pizza ready functions interact when there is 
 only one pizza at the at the beginning */ 
 
-const readline = require('readline');
-const fs = require('fs');
+
+const readline = require('readline'); //enables the readline module so that so that we can handle user input line by line.
+const fs = require('fs'); //enables the file system module so that the pizzaArray can be saved at the end. 
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+}); // defines an interface that read input from command line.
 
 const pizzaArray = [];
 let currentPizzaIndex = 0;
 let nextPizzaIndex = 1;
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-
-function displayCurrentAndNextPizzas() {
+// this function displays the pizza that is ready and the pizza being prepared.
+function displayCurrentAndNextPizzas() { 
 if (currentPizzaIndex >= pizzaArray.length) {
     console.log("\nNo more pizzas!\n");
   } else {
@@ -27,8 +28,9 @@ if (currentPizzaIndex >= pizzaArray.length) {
       console.log(`${pizzaArray[nextPizzaIndex].name}'s ${pizzaArray[nextPizzaIndex].pizza} pizza, number ${nextPizzaIndex} is now being prepared.\n`);
     }
   }
-}
+} 
 
+ //this function moves the pizza counters up as the pizzas become ready.
 function pizzaReady() {
   if (currentPizzaIndex >= pizzaArray.length) {
     console.log("No more pizzas!");
@@ -38,6 +40,10 @@ function pizzaReady() {
   }
 }
 
+/* This function uses readline to allow users to input their names and pizza preferences.
+It is recursive until stopped by typing stop into the name prompt.
+Typing log into the name prompt will display the entire pizza array.
+Typing next into the name prompt will increase the value of currentPizzaIndex and nextPizzaIndex by one.*/
 function addCustomer() {
   rl.question('Please enter your name: ', (name) => {
     if (name.toLowerCase() === "stop") {
@@ -52,7 +58,7 @@ function addCustomer() {
     } else {
       rl.question(`\nWhat pizza would you like ${name}? ` , (pizza) => {
         pizzaArray.push({ name, pizza });
-        console.log(`Thank you, ${name}! Your ${pizza} pizza has been added to the list!`);
+        console.log(`\nThank you, ${name}! Your ${pizza} pizza has been added to the list!`);
         displayCurrentAndNextPizzas();
         addCustomer();
       });
@@ -60,8 +66,9 @@ function addCustomer() {
   });
 }
 
-addCustomer()
+addCustomer()//this calls the function for the first time.
 
+//this listens for the code ending and when it does it saves the pizza array to a .json
 process.on('exit', () => {
   const data = JSON.stringify(pizzaArray);
   fs.writeFileSync('pizza-orders.json', data);
