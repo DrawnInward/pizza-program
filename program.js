82a45213ecb469d00cd42c30d83f9273aefe2d91
DrawnInward <jonathan_bean@hotmail.co.uk>
2023-03-10 11:34:02 +0000

    added a remove function but it needs adjustment as it doesn't fully work. Left comment.

--- program.js
+++ program.js
@@ -1,5 +1,5 @@
 /* things to add and sort out:
-* you need to add a way to remove people from the array
+* you need to fix the remove function so that it works with the current and next index's. Current problems are that the pizza counters are counting through the index's not the customer numbers, so it displays pizzas that have already been removed. Also new pizzas are assigned the same customer numbers because the array length gets shorter. Maybe use delete instead of splice? (doesn't work, maybe can't iterate properly over undefined element in array).
 * you need too sort out how the display current pizza and the pizza ready functions interact when there is 
 only one pizza at the beginning add order numbers to pizza array */ 
 
@@ -54,6 +54,17 @@ pizzaArray.push({ customerName, pizza, customerNumber });
 console.log(`\nThank you, ${customerName}! Your ${pizza} pizza has been added to the list! You are number ${customerNumber}.`);
 }
 
+// Current problems are that the pizza counters are counting through the index's not the customer numbers, so it displays pizzas that have already been removed. Also new pizzas are assigned the same customer numbers because the array length gets shorter. Maybe use delete instead of splice? (doesn't work, maybe can't iterate properly over undefined element in array).
+const removeCustomer = (customerNumber) => {
+  const index = pizzaArray.findIndex(x => x.customerNumber === customerNumber);
+  if (index !== -1) {
+    delete pizzaArray.splice(index, 1);
+    console.log(`Customer ${customerNumber} has been removed.`);
+  } else {
+    console.log(`No customer with number ${customerNumber} was found.`);
+  }
+};
+
 
 /* This function uses readline to allow users to input their names and pizza preferences.
 It is recursive until stopped by typing stop into the name prompt.
@@ -74,8 +85,26 @@ function addCustomer() {
       reverseCounters()
       displayCurrentAndNextPizzas();
       addCustomer()
+    } else if (customerName.toLowerCase() === "remove") {
+      rl.question(`\nPlease enter the customer number: `, (number) => {
+        let customerNumber = parseInt(number);
+        if (isNaN(customerNumber) || customerNumber <= 0 || customerNumber > pizzaArray.length) {
+          console.log(`Invalid customer number. Please try again.`);
+          addCustomer();
+        } else {
+          let removedCustomer = pizzaArray[pizzaArray.findIndex(x => x.customerNumber === customerNumber)];
+          rl.question(`Type "yes" to remove ${removedCustomer.customerName}'s ${removedCustomer.pizza} pizza, number ${removedCustomer.customerNumber}. `, (answer) => {
+            if (answer.toLowerCase() === "yes") {
+              removeCustomer(customerNumber);
+            } else {
+              console.log(`Customer ${customerNumber} was not removed.`);
+            }
+            addCustomer();
+          });
+        }
+      });
     } else {
-      rl.question(`\nWhat pizza would you like ${customerName}? ` , (pizza) => {
+      rl.question(`\nWhat pizza would you like ${customerName}? `, (pizza) => {
         updateArray(customerName, pizza)
         displayCurrentAndNextPizzas();
         addCustomer();
