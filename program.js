2d898ef9b66349c42722da3285c3bea5692cf832
DrawnInward <jonathan_bean@hotmail.co.uk>
2023-03-11 09:56:42 +0000

    started to fix the remove customer function but created another problem, lol

--- program.js
+++ program.js
@@ -1,7 +1,8 @@
 /* things to add and sort out:
 * you need to fix the remove function so that it works with the current and next index's. Current problems are that the pizza counters are counting through the index's not the customer numbers, so it displays pizzas that have already been removed. Also new pizzas are assigned the same customer numbers because the array length gets shorter. Maybe use delete instead of splice? (doesn't work, maybe can't iterate properly over undefined element in array).
 * you need too sort out how the display current pizza and the pizza ready functions interact when there is 
-only one pizza at the beginning add order numbers to pizza array */ 
+only one pizza at the beginning add order numbers to pizza array
+YOU NEED TO UPDATE THE DISPLAY CURRENT AND NEXT PIZZAS TO WORK OFF THE UNIQUE CUSTOMER NUMBER RATHER THAN THE INDEX'S!!! */ 
 
 
 const readline = require('readline'); //enables the readline module so that so that we can handle user input line by line.
@@ -14,6 +15,7 @@ const rl = readline.createInterface({
 const pizzaArray = [];
 let currentPizzaIndex = 0;
 let nextPizzaIndex = 1;
+let uniqueCustomerNumber = 0 
 
 // this function displays the pizza that is ready and the pizza being prepared. This function should also clear the terminal so that only the cirrent and next pizzas and the input are displayed.
 function displayCurrentAndNextPizzas() { 
@@ -49,7 +51,7 @@ function reverseCounters() {
 } 
 
 function updateArray(customerName, pizza) {
-let customerNumber = pizzaArray.length + 1
+let customerNumber = uniqueCustomerNumber++
 pizzaArray.push({ customerName, pizza, customerNumber });
 console.log(`\nThank you, ${customerName}! Your ${pizza} pizza has been added to the list! You are number ${customerNumber}.`);
 }
@@ -58,7 +60,7 @@ console.log(`\nThank you, ${customerName}! Your ${pizza} pizza has been added to
 const removeCustomer = (customerNumber) => {
   const index = pizzaArray.findIndex(x => x.customerNumber === customerNumber);
   if (index !== -1) {
-    pizzaArray[index] = { name: "X", pizza: "X", customerNumber: "X" };
+    pizzaArray.splice(index, 1)
     console.log(`Customer ${customerNumber} has been removed.`);
   } else {
     console.log(`No customer with number ${customerNumber} was found.`);
@@ -99,8 +101,8 @@ function addCustomer() {
             } else {
               console.log(`Customer ${customerNumber} was not removed.`);
             }
+            displayCurrentAndNextPizzas();
             addCustomer();
-            displayCurrentAndNextPizzas()
           });
         }
       });
