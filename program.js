14488ba1af1109e10b02c877da5595219f77dd4b
DrawnInward <jonathan_bean@hotmail.co.uk>
2023-03-13 09:45:01 +0000

    made the remove function into a pure function

--- program.js
+++ program.js
@@ -1,8 +1,5 @@
 /* things to add and sort out:
-* you need to fix the remove function so that it works with the current and next index's. Current problems are that the pizza counters are counting through the index's not the customer numbers, so it displays pizzas that have already been removed. Also new pizzas are assigned the same customer numbers because the array length gets shorter. Maybe use delete instead of splice? (doesn't work, maybe can't iterate properly over undefined element in array).
-* you need too sort out how the display current pizza and the pizza ready functions interact when there is 
-only one pizza at the beginning add order numbers to pizza array
-YOU NEED TO UPDATE THE DISPLAY CURRENT AND NEXT PIZZAS TO WORK OFF THE UNIQUE CUSTOMER NUMBER RATHER THAN THE INDEX'S!!! */ 
+ */ 
 
 
 const readline = require('readline'); //enables the readline module so that so that we can handle user input line by line.
@@ -42,6 +39,7 @@ function pizzaReady() {
   }
 }
 
+//this function moves the counters back.
 function reverseCounters() {
   if (currentPizzaIndex === 0) {
     console.log("\nDu kannst nicht weiter zurückgehen, Dumme.");
@@ -51,6 +49,7 @@ function reverseCounters() {
   }
 } 
 
+//this function creates a customer object within the pizzaArray
 function updateArray(customerName, pizza) {
 let customerNumber = uniqueCustomerNumber++
 pizzaArray.push({ customerName, pizza, customerNumber });
@@ -58,15 +57,28 @@ console.log(`\nThank you, ${customerName}! Your ${pizza} pizza has been added to
 }
 
 // This function removes customers from the list when inputted with a valid customer number.
-const removeCustomer = (customerNumber) => {
-  const index = pizzaArray.findIndex(x => x.customerNumber === customerNumber);
-  if (index !== -1) {
-    pizzaArray.splice(index, 1)
-    console.log(`Customer ${customerNumber} has been removed.`);
+function removeCustomer(number) {
+  let removedCustomerNumber = parseInt(number);
+  let removedCustomerIndex = pizzaArray.findIndex(x => x.customerNumber === removedCustomerNumber) 
+  if (isNaN(removedCustomerNumber) || removedCustomerNumber <= 0 || removedCustomerNumber > pizzaArray.length || removedCustomerIndex === -1) {
+    console.log(`Invalid customer number. Please try again.`);
+    displayCurrentAndNextPizzas()
+    addCustomer();
   } else {
-    console.log(`No customer with number ${customerNumber} was found.`);
+    let removedCustomer = pizzaArray[removedCustomerIndex];
+    rl.question(`Type "yes" to remove ${removedCustomer.customerName}'s ${removedCustomer.pizza} pizza, number ${removedCustomer.customerNumber}. `, (answer) => {
+      if (answer.toLowerCase() === "yes") {
+        pizzaArray.splice(removedCustomerIndex, 1)
+        console.log(`Customer ${removedCustomerNumber} has been removed.`);
+      } else {
+        console.log(`Customer number ${removedCustomerNumber} was not removed.`);
+      }
+      displayCurrentAndNextPizzas();
+      addCustomer();
+    })
   }
 };
+
 
 
 /* This function uses readline to allow users to input their names and pizza preferences.
@@ -90,25 +102,8 @@ function addCustomer() {
       addCustomer()
     } else if (customerName.toLowerCase() === "remove") {
       rl.question(`\nPlease enter the customer number: `, (number) => {
-        let customerNumber = parseInt(number);
-        // you need to change the last bit of this line to check if the customer number actually exists, then i think it will work.
-        let removedCustomerIndex = pizzaArray.findIndex(x => x.customerNumber === customerNumber) 
-        if (isNaN(customerNumber) || customerNumber <= 0 || customerNumber > pizzaArray.length || removedCustomerIndex === -1) { // you need to change the last bit of this line to check if the customer number actually exists, then i think it will work.
-          console.log(`Invalid customer number. Please try again.`);
-          addCustomer();
-        } else {
-          let removedCustomer = pizzaArray[removedCustomerIndex];
-          rl.question(`Type "yes" to remove ${removedCustomer.customerName}'s ${removedCustomer.pizza} pizza, number ${removedCustomer.customerNumber}. `, (answer) => {
-            if (answer.toLowerCase() === "yes") {
-              removeCustomer(customerNumber);
-            } else {
-              console.log(`Customer ${customerNumber} was not removed.`);
-            }
-            displayCurrentAndNextPizzas();
-            addCustomer();
-          });
-        }
-      });
+        removeCustomer(number)
+      })
     } else {
       rl.question(`\nWhat pizza would you like ${customerName}? `, (pizza) => {
         updateArray(customerName, pizza)
@@ -119,7 +114,7 @@ function addCustomer() {
   });
 }
 
-addCustomer()//this calls the function for the first time.
+    addCustomer()//this calls the function for the first time.
 
 //this listens for the code ending and when it does it saves the pizza array to a .json
 process.on('exit', () => {
